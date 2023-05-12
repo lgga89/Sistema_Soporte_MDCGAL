faafc12f33c8dc715366d2c0c5faa86316227c00
lgga89 <lg_20_GA@hotmail.com>
2023-05-12 16:48:49 -0500

    validar button
    aun falta corregir

--- js/validar.js
+++ js/validar.js
@@ -72,38 +72,18 @@ const validarFormulario = (e) => {
 
 const validarCampo = (expresion, input, campo) => {
   if (expresion.test(input.value)) {
-    document
-      .getElementById(`grupo__${campo}`)
-      .classList.remove("formulario__grupo-incorrecto");
-    document
-      .getElementById(`grupo__${campo}`)
-      .classList.add("formulario__grupo-correcto");
-    document
-      .querySelector(`#grupo__${campo} i`)
-      .classList.add("fa-check-circle");
-    document
-      .querySelector(`#grupo__${campo} i`)
-      .classList.remove("fa-times-circle");
-    document
-      .querySelector(`#grupo__${campo} .formulario__input-error`)
-      .classList.remove("formulario__input-error-activo");
+    document.getElementById(`grupo__${campo}`).classList.remove("formulario__grupo-incorrecto");
+    document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-correcto");
+    document.querySelector(`#grupo__${campo} i`).classList.add("fa-check-circle");
+    document.querySelector(`#grupo__${campo} i`).classList.remove("fa-times-circle");
+    document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo");
     campos[campo] = true;
   } else {
-    document
-      .getElementById(`grupo__${campo}`)
-      .classList.add("formulario__grupo-incorrecto");
-    document
-      .getElementById(`grupo__${campo}`)
-      .classList.remove("formulario__grupo-correcto");
-    document
-      .querySelector(`#grupo__${campo} i`)
-      .classList.add("fa-times-circle");
-    document
-      .querySelector(`#grupo__${campo} i`)
-      .classList.remove("fa-check-circle");
-    document
-      .querySelector(`#grupo__${campo} .formulario__input-error`)
-      .classList.add("formulario__input-error-activo");
+    document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-incorrecto");
+    document.getElementById(`grupo__${campo}`).classList.remove("formulario__grupo-correcto");
+    document.querySelector(`#grupo__${campo} i`).classList.add("fa-times-circle");
+    document.querySelector(`#grupo__${campo} i`).classList.remove("fa-check-circle");
+    document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");
     campos[campo] = false;
   }
 };
@@ -134,24 +114,16 @@ formulario.addEventListener("submit", (e) => {
   ) {
     formulario.reset();
 
-    document
-      .getElementById("formulario__mensaje-exito")
-      .classList.add("formulario__mensaje-exito-activo");
+    document.getElementById("formulario__mensaje-exito").classList.add("formulario__mensaje-exito-activo");
     setTimeout(() => {
-      document
-        .getElementById("formulario__mensaje-exito")
-        .classList.remove("formulario__mensaje-exito-activo");
+      document.getElementById("formulario__mensaje-exito").classList.remove("formulario__mensaje-exito-activo");
     }, 5000);
 
-    document
-      .querySelectorAll(".formulario__grupo-correcto")
-      .forEach((icono) => {
+    document.querySelectorAll(".formulario__grupo-correcto").forEach((icono) => {
         icono.classList.remove("formulario__grupo-correcto");
       });
   } else {
-    document
-      .getElementById("formulario__mensaje")
-      .classList.add("formulario__mensaje-activo");
+    document.getElementById("formulario__mensaje").classList.add("formulario__mensaje-activo");
   }
 });
 
