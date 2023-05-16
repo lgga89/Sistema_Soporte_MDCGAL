3e21d622980f1cdffc40d3cdc5208d8460a8ee12
lgga89 <lg_20_GA@hotmail.com>
2023-05-16 16:54:29 -0500

    validar 05
    no puedo

--- js/validacion.js
+++ js/validacion.js
@@ -14,16 +14,14 @@ const fecha = document.getElementById("fecha");
 
 
 
-form.addEventListener('submit', e => {
+fomrulario.addEventListener('submit', e => {
     e.preventDefault();
-
     validateInputs();
 });
 
 const setError = (element, message) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
-
     errorDisplay.innerText = message;
     inputControl.classList.add('error');
     inputControl.classList.remove('success')
@@ -32,7 +30,6 @@ const setError = (element, message) => {
 const setSuccess = element => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
-
     errorDisplay.innerText = '';
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
@@ -47,28 +44,81 @@ const iscodPatri = dni => {
   return re.test(String(codPatri).toLowerCase());
 }
 
-/*
-function isDni(dni) {
-  return /^.{1,8}$/.test(dni);
-}
-function iscodPatri(codPatri) {
-  return /^.{1,12}$/.test(codPatri);
-}
 
-*/
 
 
 const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+ 
+
+    const idValue = id.value.trim();
+    const nombreEncargadoValue = nombreEncargado.value.trim();
+    const dniValue = dni.value.trim();
+    const sedeValue = sede.value.trim();
+    const cargoValue = cargo.value.trim();
+    const oficinaValue = oficina.value.trim();
+    const solicitanteValue = solicitante.value.trim();
+    const categoriaValue = categoria.value.trim();
+    const codPatriValue = codPatri.value.trim();
+    const problematicaValue = problematica.value.trim();
+    const fechaValue = fecha.value.trim();
 
 
-
-
-
-
+    if (nombreEncargadoValue === "") {
+      setErrorFor(nombreEncargado, "Password cannot be blank");
+    } else {
+      setSuccessFor(nombreEncargado);
+    }
+    
+    if (dniValue === "") {
+      setErrorFor(dni, "Password cannot be blank");
+    } else {
+      setSuccessFor(dni);
+    }
+    
+    if (sedeValue === "") {
+      setErrorFor(sede, "Password cannot be blank");
+    } else {
+      setSuccessFor(sede);
+    }
+    
+    if (cargoValue === "") {
+      setErrorFor(cargo, "Password cannot be blank");
+    } else {
+      setSuccessFor(cargo);
+    }
+    
+    if (oficinaValue === "") {
+      setErrorFor(oficina, "Password cannot be blank");
+    } else {
+      setSuccessFor(oficina);
+    }
+    if (solicitanteValue === "") {
+      setErrorFor(solicitante, "Password cannot be blank");
+    } else {
+      setSuccessFor(solicitante);
+    }
+    if (categoriaValue === "") {
+      setErrorFor(categoria, "Password cannot be blank");
+    } else {
+      setSuccessFor(categoria);
+    }
+    if (codPatriValue === "") {
+      setErrorFor(codPatri, "Password cannot be blank");
+    } else {
+      setSuccessFor(codPatri);
+    }
+    if (problematicaValue === "") {
+      setErrorFor(problematica, "Password cannot be blank");
+    } else {
+      setSuccessFor(problematica);
+    }
+    if (fechaValue === "") {
+      setErrorFor(fecha, "Password cannot be blank");
+    } else {
+      setSuccessFor(fecha);
+    }
+    
+/*
     if(usernameValue === '') {
         setError(username, 'Username is required');
     } else {
@@ -98,105 +148,7 @@ const validateInputs = () => {
     } else {
         setSuccess(password2);
     }
+*/
 
 };
 
-
-
-
-
-
-
-
-
-
-
-
-if (nombreEncargadoValue === "") {
-  setErrorFor(nombreEncargado, "Password cannot be blank");
-} else {
-  setSuccessFor(nombreEncargado);
-}
-
-if (dniValue === "") {
-  setErrorFor(dni, "Password cannot be blank");
-} else {
-  setSuccessFor(dni);
-}
-
-if (sedeValue === "") {
-  setErrorFor(sede, "Password cannot be blank");
-} else {
-  setSuccessFor(sede);
-}
-
-if (cargoValue === "") {
-  setErrorFor(cargo, "Password cannot be blank");
-} else {
-  setSuccessFor(cargo);
-}
-
-if (oficinaValue === "") {
-  setErrorFor(oficina, "Password cannot be blank");
-} else {
-  setSuccessFor(oficina);
-}
-if (solicitanteValue === "") {
-  setErrorFor(solicitante, "Password cannot be blank");
-} else {
-  setSuccessFor(solicitante);
-}
-if (categoriaValue === "") {
-  setErrorFor(categoria, "Password cannot be blank");
-} else {
-  setSuccessFor(categoria);
-}
-if (codPatriValue === "") {
-  setErrorFor(codPatri, "Password cannot be blank");
-} else {
-  setSuccessFor(codPatri);
-}
-if (problematicaValue === "") {
-  setErrorFor(problematica, "Password cannot be blank");
-} else {
-  setSuccessFor(problematica);
-}
-if (fechaValue === "") {
-  setErrorFor(fecha, "Password cannot be blank");
-} else {
-  setSuccessFor(fecha);
-}
-
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
-  formControl.className = "form-control error";
-  small.innerText = message;
-}
-
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control success";
-}
-
-function isDni(dni) {
-  return /^.{1,8}$/.test(dni);
-}
-function iscodPatri(codPatri) {
-  return /^.{1,12}$/.test(codPatri);
-}
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector(".floating-btn");
-const close_btn = document.querySelector(".close-btn");
-const social_panel_container = document.querySelector(
-  ".social-panel-container"
-);
-
-floating_btn.addEventListener("click", () => {
-  social_panel_container.classList.toggle("visible");
-});
-
-close_btn.addEventListener("click", () => {
-  social_panel_container.classList.remove("visible");
-});
